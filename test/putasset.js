0bd9a777d3f1a796d53124c30bfae61b4eee3b0d
coderaiser <coderaiser@cloudcmd.io>
2018-11-30 19:01:43 +0200

    fix(puttaset) mime-types

--- test/putasset.js
+++ test/putasset.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const test = require('tape');
+const tryToCatch = require('try-to-catch');
+const tryTo = require('try-to-tape');
+const test = tryTo(require('tape'));
 const putasset = require('..');
 
 const empty = () => {};
@@ -8,47 +10,39 @@ const empty = () => {};
 const owner = 'coderaiser';
 const repo = 'putasset';
 const tag = 'v1.0.0';
-const filename = 'release.zip';
 
-test('arguments: token', (t) => {
-    const fn = () => putasset(123, {owner, repo, tag});
+test('arguments: token', async (t) => {
+    const [e] = await tryToCatch(putasset, 123, {owner, repo, tag});
     
-    t.throws(fn, /token must to be a string!/, 'should throw when token not string');
+    t.equal(e.message, 'token must to be a string!', 'should throw when token not string');
     t.end();
 });
 
-test('arguments: callback', (t) => {
-    const fn = () => putasset('', {owner, repo, tag, filename});
+test('arguments: owner', async (t) => {
+    const [e] = await tryToCatch(putasset, '', {repo}, empty);
     
-    t.throws(fn, /callback must to be function!/, 'should throw when no callback');
+    t.equal(e.message, 'owner must to be a string!', 'should throw when token not string');
     t.end();
 });
 
-test('arguments: owner', (t) => {
-    const fn = () => putasset('', {repo}, empty);
+test('arguments: repo', async (t) => {
+    const [e] = await tryToCatch(putasset, '', {owner}, empty);
     
-    t.throws(fn, /owner must to be a string!/, 'should throw when token not string');
+    t.equal(e.message, 'repo must to be a string!', 'should throw when repo not string');
     t.end();
 });
 
-test('arguments: repo', (t) => {
-    const fn = () => putasset('', {owner}, empty);
+test('arguments: tag', async (t) => {
+    const [e] = await tryToCatch(putasset, '', {owner, repo}, empty);
     
-    t.throws(fn, /repo must to be a string!/, 'should throw when repo not string');
+    t.equal(e.message, 'tag must to be a string!', 'should throw when tag not string');
     t.end();
 });
 
-test('arguments: tag', (t) => {
-    const fn = () => putasset('', {owner, repo}, empty);
+test('arguments: filename', async (t) => {
+    const [e] = await tryToCatch(putasset, '', {owner, repo, tag}, empty);
     
-    t.throws(fn, /tag must to be a string!/, 'should throw when tag not string');
-    t.end();
-});
-
-test('arguments: filename', (t) => {
-    const fn = () => putasset('', {owner, repo, tag}, empty);
-    
-    t.throws(fn, /filename must to be a string!/, 'should throw when filename not string');
+    t.equal(e.message, 'filename must to be a string!', 'should throw when filename not string');
     t.end();
 });
 
